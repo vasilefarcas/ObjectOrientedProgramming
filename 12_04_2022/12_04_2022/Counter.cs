8728dea81e0eb6f5b4bc7d9e3279e7705297443e
vasilefarcas <farcasvasile06@gmail.com>
2022-04-12 12:08:53 +0300

    12_04_2022
    Add counter, multicounter

---
+++ 12_04_2022/12_04_2022/Counter.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Counter
+{
+    private string id;
+    private int count;
+    private Counter nextCounter;
+    public Counter NextCounter
+    {
+        get
+        {
+            return nextCounter;
+        }
+        set
+        {
+            nextCounter = value;
+        }
+    }
+    public int Count
+    {
+        get
+        {
+            return count;
+        }
+    }
+    public Counter(string id)
+    {
+        this.id = id;
+        nextCounter = null;
+    }
+
+    internal void Increment()
+    {
+        count++;
+        if (count == 10)
+        {
+            count = 0;
+            if (nextCounter != null)
+            {
+                nextCounter.Increment();
+            }
+        }
+    }
+    public override string ToString()
+    {
+        return $"[Counter <{id}> : {count}]";
+    }
+    internal void Reset()
+    {
+        count = 0;
+    }
+}
